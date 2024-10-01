bc833667f90c0b5e5ab83ae835e06a468b36821d
Resul Elezi <resul_elezi@protonmail.com>
2024-10-01 10:22:28 +0200

    #

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 "use strict";
 
 let currentTime = document.getElementById("current-time");
+let sleepTimeInput = document.getElementById("sleep-time-input");
+let timeOutput = document.getElementById("output-time");
+let submitBtn = document.getElementById("submit-btn");
 
+// Current time
 function showTime() {
     let now = new Date();
     let hours = now.getHours();
@@ -15,5 +19,11 @@ function showTime() {
     let timeString = hours + ":" + minutes + ":" + seconds;
     currentTime.innerHTML = timeString;
 }
-
 setInterval(showTime, 1000);
+
+// Sleep Time
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let enteredTime = sleepTimeInput.value;
+    timeOutput.textContent = enteredTime;
+});
