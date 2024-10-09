9dc39c052307b79d050fe542f9274631865de556
hafizakbar19 <hafizakbar19@gmail.com>
2024-10-09 17:02:17 +0500

    sleep amount and wake up time logic

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ let currentTime = document.getElementById("current-time");
 let sleepTimeInput = document.getElementById("sleep-time-input");
 let timeOutput = document.getElementById("output-time");
 let submitBtn = document.getElementById("submit-btn");
+let sleepAmount = document.getElementById("sleep-amount-input");
+let sleepAmountOutput = document.getElementById("output-amount");
+let sleepAmountSubmit = document.getElementById("sleep-amount-btn");
+let wakeUpTime = document.getElementById("output-wake-up");
+
 
 // Current time
 function showTime() {
@@ -25,5 +30,26 @@ setInterval(showTime, 1000);
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     let enteredTime = sleepTimeInput.value;
-    timeOutput.textContent = enteredTime;
+    timeOutput.textContent = "You will go to sleep at: " + enteredTime;
+});
+
+//sleep amount
+
+sleepAmountSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let enteredAmount = sleepAmount.value;
+    sleepAmountOutput.textContent = "You like to sleep for: "+enteredAmount+" hours";
+});
+
+//wake up time
+
+sleepAmountSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let enteredAmount = sleepAmount.value;
+    let enteredTime = sleepTimeInput.value;
+    let wakeUp = parseInt(enteredTime.split(":")[0]) + parseInt(enteredAmount);
+    if(wakeUp > 24) {
+        wakeUp = wakeUp - 24;
+    }
+    wakeUpTime.textContent = "You should wake up at: "+wakeUp+":" + enteredTime.split(":")[1];
 });
