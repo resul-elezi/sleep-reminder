41f3c3657e9ef70e671d8f97151e62bcd93c5a54
Resul Elezi <resul_elezi@protonmail.com>
2024-10-10 10:34:33 +0200

    [Feature] Add light/dark mode #2. Closes #2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 "use strict";
 
-let currentTime = document.getElementById("current-time");
-let sleepTimeInput = document.getElementById("sleep-time-input");
-let timeOutput = document.getElementById("output-time");
-let submitBtn = document.getElementById("submit-btn");
+const currentTime = document.getElementById("current-time");
+const sleepTimeInput = document.getElementById("sleep-time-input");
+const timeOutput = document.getElementById("output-time");
+const submitBtn = document.getElementById("submit-btn");
+const themeToggle = document.getElementById("toggleTheme");
+const themeIcon = document.getElementById("themeIcon");
+const logoIcon = document.getElementById("logo");
+let isDarkMode = document.documentElement.getAttribute("data-theme") === "dark";
 
 // Current time
 function showTime() {
@@ -27,3 +31,24 @@ submitBtn.addEventListener("click", (e) => {
     let enteredTime = sleepTimeInput.value;
     timeOutput.textContent = enteredTime;
 });
+
+// Light/Dark mode 
+
+function toggleTheme() {
+    isDarkMode = !isDarkMode;
+    document.documentElement.setAttribute("data-theme", isDarkMode ? "dark" : "light");
+    themeIcon.src = isDarkMode ? "assets/images/icon-sun.svg" : "assets/images/icon-moon.svg";
+    logoIcon.src = isDarkMode ? "assets/images/logo-no-background-light.svg" : "assets/images/logo-no-background.svg";
+  }
+  
+  function initializeTheme() {
+    themeIcon.src = isDarkMode ? "assets/images/icon-sun.svg" : "assets/images/icon-moon.svg";
+    logoIcon.src = isDarkMode ? "assets/images/logo-no-background-light.svg" : "assets/images/logo-no-background.svg";
+  }
+  
+  initializeTheme();
+  
+  themeToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleTheme();
+  });
